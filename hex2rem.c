f0ca406711f3ea457ec3c54a824e034a4ba527d6
Ryan Gray <ryan@graytrek.com>
2024-01-01 15:42:42 -0700

    Add line number option, fix input and output filename handling - Specify the line number with: -l nnnn - Fix using '-' for standard input and output - Output name can be optionally given

--- hex2rem.c
+++ hex2rem.c
@@ -7,52 +7,82 @@
 #include <string.h>
 #include <ctype.h>
 
-#define VERSION "1.0.1"
+#define VERSION "1.1"
 
-char *infile = "";
+char *infile = NULL;
+char *outfile = NULL;
 enum input_style {IN_HEX, IN_BINARY};
 enum input_style in_fmt = IN_HEX;
-
+int lineno = 1;
 
 void print_usage ()
   {
   printf("hex2rem %s by Ryan Gray\n", VERSION);
   printf("Make a one-line REM statement from hex codes for zmakebas input.\n");
-  printf("Usage:  hex2rem [options] infile > outfile\n");
-  printf("Options are:\n");
-  printf("  -h  Input is ASCII hex codes (default)\n");
-  printf("  -b  Input is a binary file.\n");
+  printf("Usage:  hex2rem [-?] [-h | -b] [-l nnnn] [infile [outfile]]\n");
+  printf("Options are:\n\n");
+  
+  printf("  -?        Print this help\n");
+  printf("  -h        Input is ASCII hex codes (default)\n");
+  printf("  -b        Input is a binary file.\n");
+  printf("  -l nnnn   Specify line number of REM (default is 1)\n");
   printf("The Zmakebas output will use \\{xxx} codes in the REM to preserve\n");
-  printf("the byte codes.\n");
+  printf("the byte codes. Input and output files default to standard in/out.\n");
   exit(1);
   }
 
 
 void parse_options(int argc, char *argv[])
 {
-    while ((argc > 1) && (argv[1][0] == '-'))
+    char *aptr;
+
+    while (argc > 1)
         {
-        switch (argv[1][1])
+        if (argv[1][0] == '-')
             {
-            case 'h':
-                in_fmt = IN_HEX;
-                break;
-            case 'b':
-                in_fmt = IN_BINARY;
-                break;
-            default:
-                print_usage();
-                exit(EXIT_FAILURE);
+            switch (argv[1][1])
+                {
+                case 'h':
+                    in_fmt = IN_HEX;
+                    break;
+                case 'b':
+                    in_fmt = IN_BINARY;
+                    break;
+                case 'l':
+                    if (argc < 3)
+                        {
+                        print_usage();
+                        exit(EXIT_FAILURE);
+                        }
+                    aptr = argv[2] + strlen(argv[2]) - 1;
+                    lineno = (unsigned int)strtoul(argv[2], &aptr, 0);
+                    ++argv;
+                    --argc;
+                    break;
+                case '\0':
+                    infile = argv[1];
+                    break;
+                default:
+                    print_usage();
+                    exit(EXIT_FAILURE);
+                }
+            }
+        else if (!infile)
+            {
+            infile = argv[1];
+            }
+        else if (!outfile)
+            {
+            outfile = argv[1];
+            }
+        else
+            {
+            print_usage();
+            exit(EXIT_FAILURE);
             }
 	    ++argv;
 	    --argc;
         }
-    if (argc <= 1)
-        {
-        print_usage();
-        exit(EXIT_FAILURE);
-        }
-    infile = argv[argc-1];
 }
 
 #define BUFFLEN 16384 /* Above this causes problems on DOS w/Turbo C++ 3  and is only */
@@ -60,16 +90,16 @@ void parse_options(int argc, char *argv[])
 
 int main(int argc,char *argv[])
 {
-FILE *in;
+FILE *in, *out;
 int b, h1, h2, l, n;
 char buff[BUFFLEN], *ptr;
 
 parse_options(argc, argv);
 
-if ( strcmp(infile,".") == 0 )
-
+if (!infile || strcmp(infile,"-") == 0 )
+    {
     in = stdin;
-
+    }
 else
     {
     if ( in_fmt == IN_BINARY )
@@ -79,12 +109,27 @@ else
 
     if ( in == NULL )
         {
-        fprintf(stderr, "Error: couldn't open file '%s'\n", infile);
+        fprintf(stderr, "Error: couldn't open input file '%s'\n", infile);
         exit(EXIT_FAILURE);
         }
     }
 
-printf(" 1 REM ");
+if (!outfile || strcmp(outfile,"-") == 0 )
+    {
+    out = stdout;
+    }
+else
+    {
+    out = fopen(outfile, "wt");
+
+    if ( out == NULL )
+        {
+        fprintf(stderr, "Error: couldn't open output file '%s'\n", outfile);
+        exit(EXIT_FAILURE);
+        }
+    }
+
+fprintf(out, "%d REM ", lineno);
 n = 0;
 
 while (!feof(in))
@@ -97,9 +142,9 @@ while (!feof(in))
             if (n >= WRAP)
                 {
                 n = 0;
-                printf("\\\n"); /* zmakebas continue line */
+                fprintf(out, "\\\n"); /* zmakebas continue line */
                 }
-            printf("\\{0x%02X}", b);
+            fprintf(out, "\\{0x%02X}", b);
             n++;
             }
         }
@@ -115,22 +160,23 @@ while (!feof(in))
                 h2 = toupper(*ptr);
                 if (h1 < '0' || h1 > 'F' || h2 < '0' || h2 > 'F')
                     {
-                    printf("\nInvalid hex code: %c%c\n", h1, h2);
+                    fprintf(stderr, "\nInvalid hex code: %c%c\n", h1, h2);
                     exit(EXIT_FAILURE);
                     }
                 if (n >= WRAP)
                     {
                     n = 0;
-                    printf("\\\n"); /* zmakebas continue line */
+                    fprintf(out, "\\\n"); /* zmakebas continue line */
                     }
-                printf("\\{0x%c%c}", h1, h2);
+                fprintf(out, "\\{0x%c%c}", h1, h2);
                 n++;
                 }
             }
         }
     }
-printf("\n");
+fprintf(out, "\n");
 fclose(in);
+fclose(out);
 
-exit(0);
+exit(EXIT_SUCCESS);
 }
